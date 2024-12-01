dcea0619c115f4545f5970dbf24ceec16fc49da0
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2024-12-01 15:33:15 +0530

    g876

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 const express=require('express');
+const mongoose=require('mongoose');
+const path = require('path');
+const port=process.env.port || 3000;
 
-const app=express();
 
-const port=process.env.PORT || 3000;
+const app =express();
+con
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.listen(port, () => {
